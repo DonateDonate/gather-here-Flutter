ab38bbc84fb4e243fee0ff803bc30f0bfd52fe06
doyeonKim <whqtkf12@naver.com>
2024-08-16 22:10:15 +0900

    chore: 주석추가

--- lib/auth/model/login_model.dart
+++ lib/auth/model/login_model.dart
@@ -2,6 +2,7 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'login_model.g.dart';
 
+// Post: /login Request Body
 @JsonSerializable()
 class LoginModel {
   final String identity;
